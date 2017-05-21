6443d9afd2d1328c6a3f65ca60346b194d7c7790
LarisMourullo <larismourullo@live.com>
2017-05-20 23:58:37 -0300

    :white_check_mark: Testing the factories

--- app/test/apiCommits.spec.js
+++ app/test/apiCommits.spec.js
@@ -1,14 +1,69 @@
 describe('apiCommits Factory', function() {
-	var factory;
+	var factory, httpBackend;
 
-	beforeEach(angular.mock.module('app'));
+	beforeEach(module('app'));
 
-	beforeEach(inject(function(apiCommits) {
+	beforeEach(inject(function(apiCommits, $httpBackend) {
 		factory = apiCommits;
+		httpBackend = $httpBackend;
+
+		httpBackend.whenGET("https://api.github.com/repos/mundipagg/goseq/commits").respond(
+			[{
+		    	"commit": {
+					"committer": {
+						"name": "GitHub",
+						"email": "noreply@github.com",
+						"date": "2017-05-02T17:36:11Z"
+					}
+				}
+			},
+			{
+		    	"commit": {
+					"committer": {
+						"name": "GitHub",
+						"email": "noreply@github.com",
+						"date": "2017-05-02T17:32:30Z"
+					}
+				}
+			},
+			{
+		    	"commit": {
+					"committer": {
+						"name": "GitHub",
+						"email": "noreply@github.com",
+						"date": "2017-05-02T17:30:48Z"
+					}
+				}
+			}]
+		);
 	}));
 
 	it('Factory Existente', function() {
 		expect(factory).toBeDefined();
 	});
+
+	it('Retorna Data do Commit', function () {			
+        factory.getCommits("goseq").then(function (response) {    
+			expect(response.data[0].commit.committer.date).toEqual("2017-05-02T17:36:11Z");
+		});
+
+	    httpBackend.flush();
+    });
+
+	it('Retorna Mês do Commit', function () {
+        factory.getCommits("goseq").then(function (response) {        	
+			expect(new Date(response.data[0].commit.committer.date).getMonth()).toEqual(4);
+		});
+
+	    httpBackend.flush();
+    });
+
+	it('Retorna Quantidade de Commits', function() {
+		factory.getCommits("goseq").then(function (response) {	
+			expect(response.data.length).toEqual(3);
+		});
+
+		httpBackend.flush();
+	})
 
 });
--- app/test/apiContributors.spec.js
+++ app/test/apiContributors.spec.js
@@ -1,14 +1,54 @@
 describe('apiContributors Factory', function() {
-	var factory;
+	var factory, httpBackend;
 
-	beforeEach(angular.mock.module('app'));
+	beforeEach(module('app'));
 
-	beforeEach(inject(function(apiContributors) {
+	beforeEach(inject(function(apiContributors, $httpBackend) {
 		factory = apiContributors;
+		httpBackend = $httpBackend;
+
+		httpBackend.whenGET("https://api.github.com/repos/mundipagg/goseq/contributors").respond(
+			[{
+		    	"login": "munirwanis",
+			    "contributions": 15
+			},
+			{
+		    	"login": "PMoneda",
+		    	"contributions": 5
+			},
+			{
+		    	"login": "agaldino",
+		    	"contributions": 4
+			}]
+		);
 	}));
 
 	it('Factory Existente', function() {
 		expect(factory).toBeDefined();
 	});
+
+	it('Retorna Login do Contribuidor', function () {			
+        factory.getContributors("goseq").then(function (response) {
+			expect(response.data[0].login).toEqual("munirwanis");
+		});
+
+	    httpBackend.flush();
+    });
+
+	it('Retorna Quantidade de Contribuidores', function() {
+		factory.getContributors("goseq").then(function (response) {	
+			expect(response.data.length).toEqual(3);
+		});
+
+		httpBackend.flush();
+	})
+
+	it('Retorna Quantidade de Contribuições do Usuário', function () {			
+        factory.getContributors("goseq").then(function (response) {
+			expect(response.data[0].contributions).toEqual(15);
+		});
+
+	    httpBackend.flush();
+    });
 
 });
