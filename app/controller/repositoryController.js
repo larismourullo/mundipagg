e0c92264c9def51abfc2e47b585e014d2327f4a3
Larissa Mourullo <larismourullo@live.com>
2018-02-09 02:14:05 -0200

    :lipstick: Implementing brand new style

--- app/controller/repositoryController.js
+++ app/controller/repositoryController.js
@@ -1,87 +1,77 @@
 (function () {
+  angular.module("app").controller("RepositoryController", repositoryController);
+  repositoryController.$inject = ["$http", "apiRepositories", "apiContributors", "apiCommits"];
 
-    angular
-        .module("app")
-        .controller("RepositoryController", repositoryController);
+  function repositoryController($http, apiRepositories, apiContributors, apiCommits) {
+    var vm = this;
+    vm.repository = [];
 
-    repositoryController.$inject = ["$http", "apiRepositories", "apiContributors", "apiCommits"];
-
-    function repositoryController($http, apiRepositories, apiContributors, apiCommits) {
-        var vm = this;
-        vm.repository = [];
-
-        // ------------- Início Variáveis Canvas ------------- //
-        vm.colors = ['#ef4043', '#49c3ce', '#005ca6'];
+    // ------------- Início Variáveis Canvas ------------- //
+    vm.colors = ['#082659', '#00b9b5', '#707070'];
+    vm.labels = [];
+    vm.data = [];
+    vm.datasetOverride = [
+        {
+            label: "Commits",
+            borderWidth: 3,
+            type: 'line'
+        }
+    ];
+    // ------------- Fim Variáveis Canvas ------------- //
+    
+    function updateCommit() {
+      apiCommits.getCommits(vm.repoSelect.name).then(function (response) { 
+        // Reset Array                         
         vm.labels = [];
-        vm.data = [];
-        vm.datasetOverride = [
-            {
-                label: "Commits",
-                borderWidth: 3,
-                type: 'line'
-            }
-        ];
-        // ------------- Fim Variáveis Canvas ------------- //
+        vm.data = [];  
+
+        var countCommits = 0;                           
+        var monthNames = ["January", "February", "March", 
+                          "April", "May", "June", "July",
+                          "August", "September", "October",
+                          "November", "December"];
         
-        function updateCommit() {
-            apiCommits
-                .getCommits(vm.repoSelect.name)
-                .then(function (response) { 
+        for (var i = 0; i < response.data.length; i++) { 
+          var date = new Date(response.data[i].commit.committer.date);
 
-                    // Reset Array                         
-                    vm.labels = [];
-                    vm.data = [];  
+          if (!vm.labels.includes(monthNames[date.getMonth()])) {                            
+            vm.labels.push(monthNames[date.getMonth()]);
 
-                    var countCommits = 0;                           
-                    var monthNames = ["January", "February", "March", "April", "May", "June", "July",
-                                        "August", "September", "October", "November", "December"];
-                    
-                    for (var i = 0; i < response.data.length; i++) { 
-                        var date = new Date(response.data[i].commit.committer.date);
+            if (countCommits != 0) {
+              vm.data.push(countCommits);                                                       
+            }        
 
-                        if (!vm.labels.includes(monthNames[date.getMonth()])) {                            
-                            vm.labels.push(monthNames[date.getMonth()]);
-
-                            if (countCommits != 0) {
-                                vm.data.push(countCommits);                                                       
-                            }         
-
-                            countCommits = 1; 
-                        } else {
-                            countCommits += 1;                                                      
-                        }          
-                        
-                    }
-
-                    vm.data.push(countCommits);
-                    vm.data.push(0);   
-                }
-            );
+            countCommits = 1; 
+          } else {
+            countCommits += 1;                                                      
+          }          
         }
 
-        vm.updateData = function() {
-            apiContributors
-                .getContributors(vm.repoSelect.name)
-                .then(function (response) {
-                    vm.contributorsNumber = response.data.length;                    
-                }
-            );  
+        vm.data.push(countCommits);
+        vm.data.push(0);   
+      });
+    }
 
-            updateCommit();         
+    vm.updateData = function() {
+      apiContributors
+        .getContributors(vm.repoSelect.name)
+        .then(function (response) {
+          vm.contributorsNumber = response.data.length;                    
         }
+      );  
+      updateCommit();         
+    }
 
-        function getRepository() {
-            apiRepositories
-                .allRepositories()
-                .then(function (response) {
-                    vm.repository = response.data;
-                    vm.repoSelect = vm.repository[0];
-                    vm.updateData();                    
-            });
-        }   
-
-        getRepository();    
-
+    function getRepository() {
+      apiRepositories
+        .allRepositories()
+        .then(function (response) {
+          vm.repository = response.data;
+          vm.repoSelect = vm.repository[0];
+          vm.updateData();                    
+      });
+    }   
+    
+    getRepository();    
   }
-
 })();
