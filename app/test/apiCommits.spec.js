83bf6dc18910c1a75f768f6f808280ed97f5692a
Larissa Mourullo <larismourullo@gmail.com>
2020-01-01 17:36:38 -0300

    :fire: Removing comments

--- app/test/apiCommits.spec.js
+++ app/test/apiCommits.spec.js
@@ -11,23 +11,4 @@ describe('apiCommits Factory', function() {
 		expect(factory).toBeDefined();
 	});
 
-	/*it('Retorna Data do Commit', function() {
-		factory.getCommits("goseq").then(function (response) {
-			expect(response.data[1].commit.committer.date).toEqual("2017-05-02T17:36:11Z");
-		});
-	});
-
-	it('Retorna Mês do Commit', function() {
-		factory.getCommits("goseq").then(function (response) {
-			expect(new Data(response.data[1].commit.committer.date).getMonth()).toEqual(4);
-		});
-	});
-
-	it('Retorna Quantidade de Commits', function() {
-		factory.getCommits("goseq").then(function (response) {	
-			console.log(response.data.length);	
-			expect(response.data.length).toEqual();
-		});
-	});*/
-
 });
